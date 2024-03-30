186ec39b10a5ed8fe3b41c760a692d52269f42c6
David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-03-30 18:18:41 +0000

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,252 @@
-// src/app/page.tsx
+// page.tsx
+'use client'
+import React, { ChangeEvent } from 'react';
+import './globals.css'; // Import global.css file
+import { IoIosCloseCircle } from "react-icons/io";
 
-import Image from "next/image";
-import FormFieldCreator from "../app/FormFieldCreator";
+interface FormField {
+  type: string;
+  name: string;
+  technicalName: string;
+  required: boolean;
+}
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormFieldCreator /> {/* Add the FormFieldCreator component here */}
-      
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30">
-          Form Management System&nbsp;
-          <code className="font-mono font-bold">Medicalwebapp</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/aristocratjnr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By Obuobi Ayim David
-            
-          </a>
+interface FormFieldCreatorProps {}
+
+interface FormFieldCreatorState {
+  isOpen: boolean;
+  fieldType: string;
+  fieldName: string;
+  isRequired: boolean;
+  formFields: FormField[];
+  dob: string;
+  filePreview: string | null;
+  errors: { [key: string]: string };
+}
+
+class FormFieldCreator extends React.Component<FormFieldCreatorProps, FormFieldCreatorState> {
+  constructor(props: FormFieldCreatorProps) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      fieldType: '',
+      fieldName: '',
+      isRequired: false,
+      formFields: [],
+      dob: '',
+      filePreview: null,
+      errors: {},
+    };
+  }
+
+  toggleModal = () => {
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  };
+
+  handleFieldTypeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ fieldType: e.target.value });
+  };
+
+  handleFieldNameChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const value = e.target.value;
+    const fieldType = this.state.fieldType;
+
+    if (fieldType === 'email') {
+      this.setState({ fieldName: value });
+    } else {
+      this.setState({ fieldName: value.toLowerCase().replace(/\s+/g, '_') });
+    }
+  };
+
+  handleIsRequiredChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ isRequired: e.target.checked });
+  };
+
+  handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.setState({ filePreview: reader.result as string });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  handleDobChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ dob: e.target.value });
+  };
+
+   handleSaveConfiguration = () => {
+    const { fieldType, fieldName, isRequired, formFields } = this.state;
+
+    if (!fieldType || !fieldName) {
+      this.setState({ errors: { fieldName: 'Field name and type are required' } });
+      return;
+    }
+
+    if (isRequired && !fieldName) {
+      this.setState({ errors: { fieldName: 'Field name is required' } });
+      return;
+    }
+
+    const newField: FormField = {
+      type: fieldType,
+      name: fieldName,
+      technicalName: fieldName,
+      required: isRequired,
+    };
+
+    this.setState((prevState) => ({
+      formFields: [...prevState.formFields, newField],
+      isOpen: false, // Close modal after saving configuration
+      fieldType: '',
+      fieldName: '',
+      isRequired: false,
+      filePreview: null,
+      errors: {},
+    }));
+
+    // Remove overlay by resetting the state's isOpen property
+    setTimeout(() => {
+      this.setState({ isOpen: false });
+    }, 300); // Adjust the timeout as per your transition duration
+  };
+
+  render() {
+    const { isOpen, fieldType, fieldName, isRequired, formFields, filePreview, errors } = this.state;
+
+    return (
+      <div className="relative p-4 ">
+          <div className="flex flex-col justify-center items-center mb-4  ">
+            <center><h2 className="text-2xl font-bold ">Form Management System</h2></center>
+          </div><br/>
+        <div className='z-10'>
+          <center>
+            <button
+              onClick={this.toggleModal}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  bottom-4 right-4"
+            >
+              Add fields
+            </button>
+          </center>
+        </div>
+
+        {isOpen && (
+          <div className=" absolute top-0 left-0 w-full z-50  h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+            <div className="bg-white p-8 rounded-lg w-96 ">
+            <div className='w-80 flex h-12 bg-red-500 justify-end'>
+            <button 
+            onClick={()=>{
+                this.setState({
+                  isOpen:false
+                })
+            }}>
+            <IoIosCloseCircle color='red' />
+
+            </button>
+            </div>
+              <div className="flex justify-end mb-4">
+                <button
+                  onClick={this.toggleModal}
+                  className="text-gray-600 hover:text-gray-900"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <h2 className="text-xl font-bold mb-4 text-center">Add a new Field</h2>
+              <div className="mb-4">
+                <label htmlFor="fieldType" className="block font-bold mb-1">
+                  Your Field Type:
+                </label>
+                <select
+                  id="fieldType"
+                  value={fieldType}
+                  onChange={this.handleFieldTypeChange}
+                  className="w-full p-2 border border-gray-300 rounded"
+                >
+                  <option value="">Select Field Type</option>
+                  <option value="text">Text</option>
+                  <option value="email">Email</option>
+                  <option value="number">Telephone</option>
+                  <option value="file">File Upload</option>
+                  <option value="textarea">Textarea</option>
+                  <option value="date">Date of birth</option>
+                </select>
+              </div>
+              {fieldType && (
+                <div>
+                  <label htmlFor="fieldName" className="block font-bold mb-1">
+                    Type a Field Name:
+                  </label>
+                  <input
+                    type="text"
+                    id="fieldName"
+                    value={fieldName}
+                    onChange={this.handleFieldNameChange}
+                    className="w-full p-2 border border-gray-300 rounded mb-4"
+                  />
+                </div>
+              )}
+              {fieldType === 'file' && (
+                <div className="mb-4">
+                  <label htmlFor="fileUpload" className="block font-bold mb-1">
+                    File Upload:
+                  </label>
+                  <input
+                    type="file"
+                    id="fileUpload"
+                    onChange={this.handleFileChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                  />
+                  {filePreview && (
+                    <img src={filePreview} alt="Uploaded File" className="mt-2 rounded-full h-20 w-20 object-cover" />
+                  )}
+                </div>
+              )}
+              <button
+                onClick={this.handleSaveConfiguration}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto block"
+              >
+                Save Configuration
+              </button>
+              {errors.fieldName && (
+                <p className="text-red-500 text-sm mt-2">{errors.fieldName}</p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="mt-8 absolute bottom-4 right-4">
+          {formFields.map((field, index) => (
+            <div key={index} className="mb-4">
+              <label className="block font-bold mb-1">{field.name}:</label>
+              <input
+                type={field.type}
+                className="w-full p-2 border border-gray-300 rounded"
+                placeholder={field.name}
+                required={field.required}
+              />
+              {field.required && <span className="text-red-500 text-sm"> *</span>}
+            </div>
+          ))}
         </div>
       </div>
-    </main>
-  );
+    );
+  }
 }
+
+export default FormFieldCreator;
